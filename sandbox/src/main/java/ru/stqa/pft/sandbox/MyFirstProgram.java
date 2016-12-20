d159ae85ad73fb9ebb1490ba638f71239011d08e
yutta13 <dodik13@gmail.com>
2016-12-20 23:13:19 +0300

    создан класс Point и новая функция

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,6 +6,14 @@ public class MyFirstProgram {
     hello("world");
     hello("YUtta");
 
+    Point p1 = new Point();
+    p1.x=3;
+    p1.y=2;
+    Point p2 = new Point();
+    p2.x=7;
+    p2.y=8;
+    System.out.println("расстояние между точками " + "P1(" + p1.x + "," + p1.y + ") " + "и " + "P2(" + p2.x + "," + p2.y + ")" + " = " + distance(p1,p2));
+
     Square s = new Square(5);
     System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
@@ -18,6 +26,11 @@ public class MyFirstProgram {
   public static void hello (String somebody ) {
 
     System.out.println("Hello," + somebody + "!");
+  }
+
+  public static double distance(Point p1, Point p2){
+    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+
   }
 
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by uttabondarenko on 20.12.16.
+ */
+public class Point {
+  public double x;
+  public double y;
+}
