ef97bfc6fef1b0b2c880a10cd5d62dee17e3c43b
yutta13 <dodik13@gmail.com>
2016-12-21 00:08:33 +0300

    п.4 добавлен метод

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -8,8 +8,12 @@ public class Point {
   public double y;
 
   public Point(double x, double y) {
-  this.x = x;
-  this.y = y;
-
+    this.x = x;
+    this.y = y;
   }
-}
+
+  public double distance(Point p2) {
+      return Math.sqrt(Math.pow((p2.x - this.x), 2) + Math.pow((p2.y - this.y), 2));
+    }
+  }
+
--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointDistance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointDistance.java
@@ -8,12 +8,9 @@ public class PointDistance {
     Point p1 = new Point(3,2);
     Point p2 = new Point(7,8);
 
-    System.out.println("Расстояние между точками " + "P1 (" + p1.x + "," + p1.y + ") " + "и " + "P2 (" + p2.x + "," + p2.y + ")" + " = " + distance(p1,p2));
+    System.out.println("Расстояние между точками " + "P1 (" + p1.x + "," + p1.y + ") " + "и " + "P2 (" + p2.x + "," + p2.y + ")" + " = " + p1.distance(p2));
 
   }
 
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
 
-  }
 }
