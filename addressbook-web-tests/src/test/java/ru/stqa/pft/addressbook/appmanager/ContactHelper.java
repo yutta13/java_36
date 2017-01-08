271e9647cc26438666f69991c92cc7f76dbc88ec
yutta13 <dodik13@gmail.com>
2017-01-08 15:50:30 +0300

    исправлено

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -44,7 +44,7 @@ public class ContactHelper extends HelperBase {
   }
 
   public void initContactModification() {
-    wd.findElement(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img")).click(); // изменить контакт
+    click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
 
   }
 
