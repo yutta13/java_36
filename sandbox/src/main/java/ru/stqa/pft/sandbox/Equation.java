a58b995f3efda716a3cb114efb0bf686894997e8
yutta13 <dodik13@gmail.com>
2017-01-09 21:41:27 +0300

    EquationTests add

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -0,0 +1,34 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by uttabondarenko on 09.01.17.
+ */
+public class Equation {
+  private final double a;
+  private final double b;
+  private final double c;
+
+  private  int n;
+
+  public Equation(double a, double b, double c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+
+    double d = b*b - 4*a*c;
+
+    if (d > 0) {
+     n = 2;
+    } else {
+      if (d == 0){
+        n = 1;
+      } else {
+        n = 0;
+      }
+    }
+  }
+
+  public int rootNumber() {
+    return n;
+  }
+}
