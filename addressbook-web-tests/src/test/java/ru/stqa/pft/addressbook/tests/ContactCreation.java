17f61f2c82e57d5555d39527de66657957e2ccaa
yutta13 <dodik13@gmail.com>
2017-01-10 21:56:47 +0300

    default

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreation.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreation.java
@@ -13,7 +13,7 @@ public class ContactCreation extends TestBase {
     public void testContactCreation() {
 
         app.getContactHelper().initContactCreation();
-        app.getContactHelper().fillContactForm(new ContactData("Yutta", "Bondarenko", "Moscow", "495-77777", "89992223311", "email1@mail.ru"));
+        app.getContactHelper().fillContactForm(new ContactData("Yutta", "Bondarenko", "Moscow", null, "89992223311", "email1@mail.ru"));
         app.getContactHelper().saveContact();
     }
 
