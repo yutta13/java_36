d3606aa02f9423ef1c2a38d5f36544b0bfac4c71
yutta13 <dodik13@gmail.com>
2017-01-30 12:59:27 +0300

    address

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -110,10 +110,13 @@ public class ContactHelper extends HelperBase {
       int id = Integer.parseInt(cells.get(0).findElement(By.tagName("input")).getAttribute("value"));
       String lastName = cells.get(1).getText();
       String firstName = cells.get(2).getText();
-      String allPhones = cells.get(5).getText();
+      String address = cells.get(3).getText();
       String allEmails = cells.get(4).getText();
+      String allPhones = cells.get(5).getText();
+
+
      contactCache.add(new ContactData().withId(id).withFirstname(firstName).withLastname(lastName)
-              .withAllPhones(allPhones).withAllEmails(allEmails));
+              .withAllPhones(allPhones).withAddress(address).withAllEmails(allEmails));
 
     }
     return new Contacts(contactCache);
@@ -124,6 +127,7 @@ public class ContactHelper extends HelperBase {
     initContactModificationById(contact.getId());
       String firstname = wd.findElement(By.name("firstname")).getAttribute("value");
       String lastname= wd.findElement(By.name("lastname")).getAttribute("value");
+      String address= wd.findElement(By.name("address")).getAttribute("value");
       String home= wd.findElement(By.name("home")).getAttribute("value");
       String mobil= wd.findElement(By.name("mobile")).getAttribute("value");
       String email= wd.findElement(By.name("email")).getAttribute("value");
@@ -132,7 +136,7 @@ public class ContactHelper extends HelperBase {
 
       wd.navigate().back();
       return new ContactData().withId(contact.getId()).withFirstname(firstname).withLastname(lastname)
-              .withHomephone(home).withMobil(mobil).withEmail(email).withEmail2(email2).withEmail3(email3);
+              .withHomephone(home).withMobil(mobil).withEmail(email).withEmail2(email2).withEmail3(email3).withAddress(address);
 
   }
 
