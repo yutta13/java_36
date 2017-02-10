caceb16967b87ad062b8b1122999029eaa15296e
yutta13 <dodik13@gmail.com>
2017-02-10 11:33:21 +0300

    hb

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -2,17 +2,18 @@ package ru.stqa.pft.addressbook.tests;
 
 
 import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.annotations.XStreamAlias;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
-
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -20,6 +21,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class GroupCreationTests extends TestBase {
+
+  Logger logger = LoggerFactory.getLogger(GroupCreationTests.class);
 
   @DataProvider
   public Iterator<Object[]> validGroups() throws IOException {
@@ -39,6 +42,7 @@ public class GroupCreationTests extends TestBase {
   @Test (dataProvider = "validGroups")
 
   public void testGroupCreation(GroupData group) {
+logger.info("Start test testGroupCreation");
     app.goTo().GroupPage();
     Groups before = app.group().all();
     app.group().create(group);
@@ -46,7 +50,7 @@ public class GroupCreationTests extends TestBase {
     Groups after = app.group().all();
     assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
-
+    logger.info("End test testGroupCreation");
   }
 
 
